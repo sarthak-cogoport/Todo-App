71bc2a57823488aeff720013d12bd5be92ca97e6
sarthak-cogoport <sarthak.gupta@cogoport.com>
2022-06-03 16:32:18 +0530

    CHerno

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM  from "react-dom";
+import { nanoid } from "nanoid";
+var notes = ["asdadas","Asdasda"]
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function TodoApp(){
+    const [text, setText] = useState("Initial Text");
+    const [tasks, setTasks] = useState([]);
+    const taskValueChange = (val) =>{
+        setText(val);
+    }
+
+    const addTask = ()=>{
+        setTasks([...tasks, {id:nanoid(),name:text}]);
+        setText("");
+    }
+    const onAlert = (name) => {
+        alert(name+" test");
+    }
+    return (
+        <>
+        <h1>Todo App</h1>
+        <div>
+        <input type="text" value={text} onChange={(e)=>taskValueChange(e.target.value)} placeholder="Type Here..." />
+        <button onClick={()=>addTask()}>ADD</button>
+        
+        </div>
+        <div>
+            <ul>
+            {tasks.map((note,i)=>{
+            return <li key={note.id}>{note.name}</li>
+            })}
+            </ul>
+        
+        </div>
+        </>
+    );
+}
+
+
+
+ReactDOM.render(<TodoApp/>, document.getElementById("root"));
